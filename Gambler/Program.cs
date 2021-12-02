4e2a9b77a7851388303e35f30160515b5d5e5e4e
aakashpotdar <potdaraakash@gmail.com>
2021-12-02 18:30:53 +0530

    Add project files.

---
+++ Gambler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gambler
+{
+    class Program
+    {
+        static int cash = 100;
+        static void Main(string[] args)
+        {
+            Uc1_StackEveryDay ObjUc1 = new Uc1_StackEveryDay();
+            int remainingAmount=ObjUc1.PlayGame(cash);
+            Console.WriteLine(remainingAmount);
+        }
+    }
+}
---
+++ Gambler/Uc1_StackEveryDay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler
+{
+    class Uc1_StackEveryDay
+    {
+
+        public int PlayGame(int amount)
+        {
+            int remainingAmount = 0;
+            int numberGame = 10;
+
+            for(int i = 1; i < numberGame; i++)
+            {
+                remainingAmount = remainingAmount - amount;
+            }
+            
+
+            return remainingAmount;
+        }
+
+
+    }
+}
